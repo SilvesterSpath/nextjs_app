dd3278e744b415aaddddba5dbfa2b576dc224668
SilvesterSpath <spath.sz@gmail.com>
2024-03-04 09:22:53 +0100

    add metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,13 @@
 import '@/assets/styles/globals.css';
 
+export const metadata = {
+  title: 'PropertyPulse | Find The Perfect Rental',
+  description:
+    'PropertyPulse is a platform that helps you find the perfect rental property in your area.',
+  keywords:
+    'property, rental, home, real estate, investment, real estate investment',
+};
+
 const MainLayout = ({ children }) => {
   return (
     <html lang='en'>
