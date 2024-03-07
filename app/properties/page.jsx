dccdea56dfb76e2d6b8fcf1fc8fa4196f3264449
SilvesterSpath <spath.sz@gmail.com>
2024-03-07 06:07:11 +0100

    properties.json in data folder

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,6 +1,10 @@
+'use client';
+
 import Link from 'next/link';
+import properties from '@/data/properties.json';
 
 const PropertiesPage = () => {
+  console.log(properties);
   return (
     <div>
       <h1 className='text-3xl'>Properties</h1>
