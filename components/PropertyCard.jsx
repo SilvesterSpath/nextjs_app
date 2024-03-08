88d74099a25dd5cc3d83b56ad30385c924977df7
SilvesterSpath <spath.sz@gmail.com>
2024-03-08 07:02:33 +0100

    refactoring getRateDisplay()

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -1,7 +1,18 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const PropertyCard = ({ property }) => {
+  const getRateDisplay = (rates) => {
+    if (rates.monthly) {
+      return rates.monthly + '/mo';
+    } else if (rates.weekly) {
+      return rates.weekly + '/wk';
+    } else if (rates.nightly) {
+      return rates.nightly + '/night';
+    }
+  };
+
   return (
     <div className='rounded-xl shadow-md relative'>
       <Image
@@ -18,9 +29,7 @@ const PropertyCard = ({ property }) => {
           <h3 className='text-xl font-bold'>{property.name}</h3>
         </div>
         <h3 className='absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right'>
-          {property.rates.monthly
-            ? property.rates.monthly + '/mo'
-            : property.rates.weekly + '/wk'}
+          ${getRateDisplay(property.rates)}
         </h3>
 
         <div className='flex justify-center gap-4 text-gray-500 mb-4'>
