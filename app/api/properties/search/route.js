61f89fbd2390eec682e34fc4d7cdff9c6a68edfc
SilvesterSpath <spath.sz@gmail.com>
2024-05-03 09:41:33 +0200

    PropertySeach route

---
+++ app/api/properties/search/route.js
@@ -0,0 +1,41 @@
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+
+// GET /api/properties/search
+// when I hit this route in the browser:
+//'http://localhost:3000/api/properties/search?location=new%20york&propertyType=condo'
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const { searchParams } = new URL(request.url);
+    const location = searchParams.get('location');
+    const propertyType = searchParams.get('propertyType');
+
+    // to get all fractions that a user types in the search bar
+    const locationPattern = new RegExp(location, 'i');
+
+    let query = {
+      $or: [
+        { name: locationPattern },
+        { description: locationPattern },
+        { 'location.street': locationPattern },
+        { 'location.city': locationPattern },
+        { 'location.state': locationPattern },
+        { 'location.zipcode': locationPattern },
+      ],
+    };
+
+    return new Response(JSON.stringify({ message: 'Success' }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response('Something went wrong', {
+      status: 500,
+    });
+  }
+};
---
+++ app/properties/search-results/page.jsx
@@ -0,0 +1,10 @@
+'use client';
+import { useRouter } from 'next/navigation';
+
+const SearchResults = () => {
+  const router = useRouter();
+
+  return <div>SearchResults</div>;
+};
+
+export default SearchResults;
