cad69124985034d8008ff160fb9fd1a0a1d964de
SilvesterSpath <spath.sz@gmail.com>
2024-05-07 06:48:33 +0200

    Searchbar on Properties page fit

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -11,26 +11,28 @@ const PropertiesPage = async () => {
   });
 
   return (
-    <section className='px-4 py-6'>
+    <>
       <section className='bg-blue-700 py-4 mb-4'>
         <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center'>
           <div className='text-center'></div>
           <PropertySeachFrom />
         </div>
       </section>
-      {properties.length === 0 ? (
-        <div>No properties found</div>
-      ) : (
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-          {properties.map((item, index) => (
-            <PropertyCard
-              key={item._id ? item._id.toString() : index}
-              property={item}
-            />
-          ))}
-        </div>
-      )}
-    </section>
+      <section className='px-4 py-6'>
+        {properties.length === 0 ? (
+          <div>No properties found</div>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {properties.map((item, index) => (
+              <PropertyCard
+                key={item._id ? item._id.toString() : index}
+                property={item}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    </>
   );
 };
 
