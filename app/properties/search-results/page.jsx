03d37f6657931e0275488a06a13032fc632b8c88
SilvesterSpath <spath.sz@gmail.com>
2024-05-07 06:46:44 +0200

    Searchbar on Properties page

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -54,37 +54,6 @@ const SearchResultsPage = () => {
       </section>
     </>
   );
-
-  /* return loading ? (
-    <Spinner />
-  ) : (
-    <section className='px-4 py-6'>
-      <div className='container-xl lg:container m-auto'>
-        <Link
-          href='/properties'
-          className='flex items-center text-blue-500 hover:underline mb-3'
-        >
-          <FaArrowCircleLeft className='mr-2 mb-1' /> Back to Properties
-        </Link>
-        <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>
-          {properties && properties.length === 0
-            ? 'No results found'
-            : properties.length === 1
-            ? 'Search Result'
-            : 'Search Results'}
-        </h2>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-          {loading ? (
-            <Spinner />
-          ) : (
-            properties.map((item) => (
-              <PropertyCard key={item._id} property={item} />
-            ))
-          )}
-        </div>
-      </div>
-    </section>
-  ); */
 };
 
 export default SearchResultsPage;
