020b1e7cc71a2a28a0b1cae60048b3854aee4220
SilvesterSpath <spath.sz@gmail.com>
2024-05-08 05:55:26 +0200

    Message model created

--- models/Message.js
+++ models/Message.js
@@ -1,22 +1,48 @@
 import { Schema, model, models } from 'mongoose';
 
-const MessageSchema = new Schema({
-  // logged in user
-  sender: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  // property owner
-  recipient: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
+const MessageSchema = new Schema(
+  {
+    // logged in user
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    // property owner
+    recipient: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
 
-  property: {
-    type: Schema.Types.ObjectId,
-    ref: 'Property',
-    required: true,
+    property: {
+      type: Schema.Types.ObjectId,
+      ref: 'Property',
+      required: true,
+    },
+    name: {
+      type: String,
+      required: [true, 'Please provide a name'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide an email'],
+    },
+    photo: {
+      type: String,
+    },
+    body: {
+      type: String,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
   },
-});
+  { timestamps: true }
+);
+
+// If model already exist first part, if not second part
+const Message = models.Message || model('Message', MessageSchema);
+
+export default Message;
