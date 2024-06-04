a71c5e89de318366bf92932e5f62ef4fc4937e6d
SilvesterSpath <spath.sz@gmail.com>
2024-06-04 13:25:38 +0200

    mark as new

--- components/Message.jsx
+++ components/Message.jsx
@@ -19,13 +19,20 @@ const Message = ({ message }) => {
       if (res.status === 200) {
         setIsRead(!isRead);
       }
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <>
       {' '}
       <div className='relative bg-white p-4 rounded-md shadow-md border border-gray-200'>
+        {!isRead && (
+          <div className='absolute top-2 right-2 bg-yellow-500 text-white rounded-full p-1'>
+            <span className='text-xs font-bold'>New</span>
+          </div>
+        )}
         <h2 className='text-xl mb-4'>
           <span className='font-bold'>Property Inquiry:</span>{' '}
           {message.property.name}
@@ -60,9 +67,11 @@ const Message = ({ message }) => {
         </ul>
         <button
           onClick={handleRead}
-          className='mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md'
+          className={`mt-4 mr-3 ${
+            isRead ? 'bg-gray-500' : 'bg-blue-500 text-white'
+          }  py-1 px-3 rounded-md`}
         >
-          {isRead ? 'Mark as Unread' : 'Mark as Read'}
+          {isRead ? 'Mark as New' : 'Mark as Read'}
         </button>
         <button className='mt-4 bg-red-500 text-white py-1 px-3 rounded-md'>
           Delete
